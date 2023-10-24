c8d328f15941e5e191f55a91bb36937eb178ecb6
Pratik8102 <leetcodesharma@gmail.com>
2023-10-24 09:46:12 +0530

    hello javascript

---
+++ app.js
@@ -0,0 +1,10 @@
+
+setTimeout(()=>{
+    document.body.style.background="red";
+},1000)
+setTimeout(()=>{
+    document.body.style.background="orange";
+},2000)
+setTimeout(()=>{
+    document.body.style.background="yellow";
+},3000)
